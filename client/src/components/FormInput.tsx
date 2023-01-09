58a107da9366f5c6583292a949362544f43f99af
Kaan Keskin <keskinkaan@gmail.com>
2023-01-09 19:38:01 +0300

    Create FormInput component and added to Form component. Created dynamically and testing.

---
+++ client/src/components/FormInput.tsx
@@ -0,0 +1,11 @@
+import { TFormElement } from '@/types/FormType';
+
+const FormInput = (props: TFormElement) => {
+	return (
+		<div className="form-body">
+			<label className="form-body__label" htmlFor=""></label>
+		</div>
+	);
+};
+
+export default FormInput;
--- client/src/pages/Form.tsx
+++ client/src/pages/Form.tsx
@@ -1,14 +1,48 @@
+import FormInput from '@/components/FormInput';
 import useFetch from '@/hooks/useFetch';
 import { TForm } from '@/types/FormType';
+import { isObject } from '@/utils/functions';
+import Logger, { LoggerType } from '@/utils/Logger';
+import { SyntheticEvent } from 'react';
 
 const Form = () => {
 	const [loading, error, form] = useFetch<TForm>('src/api/form.json');
+
+	const handleSubmit = (e: SyntheticEvent<HTMLFormElement, SubmitEvent>) => {
+		e.preventDefault();
+		const btn = e.nativeEvent.submitter;
+		const cls = btn?.className;
+		if (!cls) return;
+
+		if (cls.indexOf('preview') >= 0) {
+			Logger.log('Preview', LoggerType.INFO);
+		} else {
+			Logger.log('Submit', LoggerType.INFO);
+		}
+	};
 
 	return (
 		<>
 			{!loading ? (
 				!error ? (
-					<div></div>
+					<form className="form" onSubmit={handleSubmit}>
+						<h2 className="form-header">{isObject<TForm>(form) ? form.formName : ''}</h2>
+						{isObject<TForm>(form) ? (
+							form.fields.map((field, i) => <FormInput key={i} {...field} />)
+						) : (
+							<div className="form-body">
+								<p>Something went wrong. Please try again later.</p>
+							</div>
+						)}
+						<div className="form-footer">
+							<button className="form-footer__preview" type="submit">
+								Preview
+							</button>
+							<button className="form-footer__submit" type="submit">
+								Submit
+							</button>
+						</div>
+					</form>
 				) : (
 					<p>Something went wrong. Please try again later.</p>
 				)
